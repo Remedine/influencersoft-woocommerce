a9ea29f80cfc96d31adaef10e1840dafac4dd539
Timothy Hill <tim@digitalseeds.dev>
2020-05-11 11:40:41 -0400

    initialize
    initialize plugin

---
+++ influencersoft_connection.php
@@ -0,0 +1,24 @@
+<?php
+
+/* Plugin Name: Influencersoft-Woocommerce API Integrator
+* Plugin URI: https://digitalseeds.dev
+ * Description: If you are using influencersoft and want to integrate it with woocommerce this is the test plugin for you.
+ * Author: Timothy Hill
+ * Version: 1.0
+ * Author URI: https://digitalseeds.dev
+ */
+
+namespace Digitalseeds;
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit( 'Cheatin&#8217; uh?' );
+}
+
+define( 'Influencersoft-woocommerce_url', plugin_dir_url( __FILE__ ) );
+
+class influencersoft_connection {
+
+	public function get_secret_code() {
+
+	}
+}
---
+++ secretcode.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Description.
+ *
+ * @package Digitalseeds
+ * @Since 1.0.0
+ * @author Timothy Hill
+ * @link https://digitalseeds.dev
+ * @license
+ */
+namespace Digitalseeds;
+
+class secret_code {
+	private function define_secret() {
+		$secret = '';
+		return $secret;
+	}
+
+}
